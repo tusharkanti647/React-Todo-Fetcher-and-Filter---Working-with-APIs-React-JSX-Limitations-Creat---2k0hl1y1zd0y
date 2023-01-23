8023270437abed6e789567c071c759c79f3de981
tusharkanti647 <107229334+tusharkanti647@users.noreply.github.com>
2023-01-23 11:04:20 +0530

    add class name in todo text

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,7 +5,7 @@ export const Todo = (props) => {
     return (
         <li>
             <div className="todo" id={props.id}>
-                <div className="todo-title">{props.title}</div>
+                <div className="todo-text">{props.title}</div>
                 <div className="todo-status">{props.completed ? "Completed" : "InCompleted"}</div>
             </div>
         </li>
